0bd046a5d727335b6976b3a6058d20aa7e65d275
JessesCUkaegbu <ukaegbujesses@gmail.com>
2023-06-22 17:54:13 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -25,3 +25,17 @@ long largest_prime_factor(long number)
 
 	return (number);
 }
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long number = 612852475143;
+	long largest_factor = largest_prime_factor(number);
+
+	printf("%ld\n", largest_factor);
+
+	return (0);
+}
