46c4b543750829c68bca58fc3e8d92ad6e326943
JessesCUkaegbu <ukaegbujesses@gmail.com>
2023-06-26 16:50:43 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenghth of a string
+ * @s: String
+ * Return: length
+*/
+void puts_half(char *str)
+{
+	int long1 = 0;
+
+	while (*s != '\0')
+	{
+		long1++;
+		s++;
+	}
+
+	return (long1);
+}
