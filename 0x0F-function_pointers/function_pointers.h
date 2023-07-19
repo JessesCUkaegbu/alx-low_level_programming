7db8e3c5f4b75f5e9c84dbe331df3846edd5bec7
JessesCUkaegbu <ukaegbujesses@gmail.com>
2023-07-19 16:29:21 -0700

    0-print_name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#define <stdlib.h>
-#define <stddef.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
